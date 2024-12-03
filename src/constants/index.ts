145a93d04064c37a5bbc2afa1da6c588234d3876
Frosty <85770158+ShaneDevelop@users.noreply.github.com>
2024-12-03 09:03:55 -0700

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,5 @@
 export const navItems = [
     { name: "Portfolio", link: "#main" },
     { name: "Projects", link: "#projects" },
-    { name: "Creations", link: "#creations" },
     { name: "Contact", link: "#contact" },
 ];
